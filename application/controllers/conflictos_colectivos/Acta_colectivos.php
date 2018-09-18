08d4facd4bcaa1b539e8bfe3eedf6e38ab34ce9a
Moisés Herrera <moises.oct@gmail.com>
2018-09-18 15:35:44 -0600

    Acta Solicitud de diferencias laborales
    incompleta

--- application/controllers/conflictos_colectivos/Acta_colectivos.php
+++ application/controllers/conflictos_colectivos/Acta_colectivos.php
@@ -5,12 +5,55 @@ class Acta_colectivos extends CI_Controller {
 
     function __construct(){
         parent::__construct();
-        $this->load->model( array('expediente_cc_model','expedientes_model'));
+        $this->load->model( array('expediente_cc_model','expedientes_model','directivos_model'));
     }
 
     public function generar_acta($id_expedienteci) {
         $expediente = $this->expediente_cc_model->expedientes_diferencia_laboral( $id_expedienteci )->result()[0];
         $empresa = $this->expedientes_model->obtener_municipio($expediente->id_empresaci);
+        $this->load->library("phpword");
+
+        $PHPWord = new PHPWord();
+
+        $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sirct/files/templates/templateDocSRCCT/FichaSolicitud_DifL.docx');
+        $templateWord->setValue('no_expediente', $expediente->numerocaso_expedienteci);
+        $templateWord->setValue('fecha_actual', date('d/m/Y'));
+        $templateWord->setValue('direccion_empresa', $empresa->direccion_empresa);
+        $templateWord->setValue('representante_legal', $empresa->nombres_representante);
+        $templateWord->setValue('especificacion', $empresa->actividad_catalogociiu);
+        $templateWord->setValue('actividad', $empresa->grupo_catalogociiu);
+        $templateWord->setValue('nombre_sindicato', $expediente->nombre_sindicato);
+        $templateWord->setValue('telefono_sindicato', $expediente->telefono_sindicato);
+        $templateWord->setValue('direccion_sindicato', $expediente->direccion_sindicato);
+        $templateWord->setValue('nombre_delegado',$expediente->delegado);
+
+        $nombreWord = $this->random();
+
+        $templateWord->saveAs($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        $phpWord2 = \PhpOffice\PhpWord\IOFactory::load($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename='FichaSolicitud_DifL_".date('dmy_His').".docx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord2, 'Word2007');
+        $objWriter->save('php://output');
+
+        unlink($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+    }
+
+    public function generar_acta_solicitud($id_expedienteci) {
+        $expediente = $this->expediente_cc_model->expedientes_diferencia_laboral( $id_expedienteci )->result()[0];
+        $empresa = $this->expedientes_model->obtener_municipio($expediente->id_empresaci);
+        $directivos = $this->directivos_model->obtener_directivos_sindicato($expediente->id_sindicato);
+        $concat_directivos='',
+        foreach ($directivos as $d) {
+          $concat_directivos.= $d->nombre_directivo.', identificándose por medio de su respectivo Documento Único de Identidad número '.
+          convertir_dui($d->dui_directivo).', actuando en su calidad de '.$d->tipo_directivo.', ';
+        }
+
         $this->load->library("phpword");
 
         $PHPWord = new PHPWord();
